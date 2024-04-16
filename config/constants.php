3af102663a31a06308f627088396c2eb188698e9
DEEPAK KUMAR <78249949+kumardeepak85690@users.noreply.github.com>
2024-04-16 11:59:47 +0530

    first commit

---
+++ config/constants.php
@@ -0,0 +1,13 @@
+<?php 
+//Start Session
+session_start();
+
+//Create Constants to save Database Credentials
+define('LOCALHOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'task_manager_db');
+
+define('SITEURL', 'http://localhost:8080/task-manager-master/');
+
+?>
